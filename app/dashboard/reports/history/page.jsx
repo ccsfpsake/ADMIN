dfd98ea3e31df600a4080d603cf17355cdadc655
ccsfpsake <ccsfpsake@gmail.com>
2025-07-12 13:40:29 +0800

    07-12-2025 140pm

--- app/dashboard/reports/history/page.jsx
+++ app/dashboard/reports/history/page.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { Suspense } from "react";
 import AdminReportHistoryPage from "./AdminReportHistoryPage";
 
 const Page = () => {
-  return <AdminReportHistoryPage />;
+  return (
+    <Suspense fallback={<div>Loading Report History...</div>}>
+      <AdminReportHistoryPage />
+    </Suspense>
+  );
 };
 
 export default Page;
