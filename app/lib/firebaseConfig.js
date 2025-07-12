a6e7427821077f3a537b3e4dc06ff2e55cf1d579
ccsfpsake <ccsfpsake@gmail.com>
2025-07-12 10:38:16 +0800

    07-12-2025 2nd

--- app/lib/firebaseConfig.js
+++ app/lib/firebaseConfig.js
@@ -5,12 +5,12 @@ import { getStorage, getDownloadURL } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDioU7gqrkZw3-hyABOjcVK64w1VwXiXm0",
-  authDomain: "sake-41894.firebaseapp.com",
-  projectId: "sake-41894",
-  storageBucket: "sake-41894.firebasestorage.app",
-  messagingSenderId: "72853475590",
-  appId: "1:72853475590:web:621245362a78ccd0194e87"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 
